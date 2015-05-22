fa87ac013cb37e79006218feb3d699c8cde31f14
kuisatz <kuisatzhaderah@gmail.com>
2015-05-22 16:11:00 +0300

    Hmac.php de eksik  yazılmıs this duzeltildi

--- vendor/hmac/Hmac.php
+++ vendor/hmac/Hmac.php
@@ -82,8 +82,8 @@ class Hmac {
      * @version 0.0.1
      */
       public function differenceTimeStamp() {
-        if(getTimeStamp() != null) {
-            return time() - getTimeStamp();
+        if($this->getTimeStamp() != null) {
+            return time() - $this->getTimeStamp();
          } else {
             return null;
         } 
