b9e7471d433584e7da5abd675ec760f1d50006a6
corner82 <311corner82@gmail.com>
2015-10-02 14:51:17 +0300

    "Cross-origion resource sharing" kontrolü
    "Cross-origion resource sharing" kontrolüne izin verilmesi için
    eklenmiştir

--- index.php
+++ index.php
@@ -20,6 +20,15 @@ $app = new \Slim\SlimExtended(array(
     'exceptions.rabbitMQ.queue.name' => \Slim\SlimExtended::EXCEPTIONS_RABBITMQ_QUEUE_NAME
     ));
 
+/**
+ * "Cross-origion resource sharing" kontrolüne izin verilmesi için eklenmiştir
+ * @author Mustafa Zeynel Dağlı
+ * @since 2.10.2015
+ */
+$res = $app->response();
+$res->header('Access-Control-Allow-Origin', '*');
+$res->header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+
 //$app->add(new \Slim\Middleware\MiddlewareTest());
 $app->add(new \Slim\Middleware\MiddlewareHMAC());
 
