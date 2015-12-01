5ff2d8abdfca0a49fcec4e1a21fbed877d0544c7
corner82 <311corner82@gmail.com>
2015-12-01 11:51:40 +0200

    try catch bloğu eklendi

--- vendor/services/database/postgresql/PostgreSQLConnectPDO.php
+++ vendor/services/database/postgresql/PostgreSQLConnectPDO.php
@@ -8,13 +8,17 @@ class PostgreSQLConnectPDO implements \Zend\ServiceManager\FactoryInterface {
     
     
     public function createService(\Zend\ServiceManager\ServiceLocatorInterface $serviceLocator) {
-        /*return $pdo = new \PDO('pgsql:dbname=ecoman_01_10;host=88.249.18.205;'
-                                . 'user=postgres;'
-                                . 'password=1q2w3e4r');*/
-        return $pdo = new \PDO('pgsql:dbname=ecoman_01_10;host=88.249.18.205;',
-                                'postgres',
-                                '1q2w3e4r',
-                                PostgreSQLConnectPDOConfig::getConfig());
+        try {
+            $pdo = new \PDO('pgsql:dbname=ecoman_01_10;host=88.249.18.205;',
+                            'postgres',
+                            '1q2w3e4r',
+                            PostgreSQLConnectPDOConfig::getConfig());
+            return $pdo;
+        } catch (PDOException $e) {
+            return false;
+        } 
+
+
     }
 
 }
