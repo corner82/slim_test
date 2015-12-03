2787f324a72a8e74450152a67fd7d236bd113dd3
corner82 <311corner82@gmail.com>
2015-12-03 13:31:52 +0200

    Filter service adları için constant değişkenleri tanımlandı
    Filter service adları için constant değişkenleri tanımlandı

---
+++ vendor/services/filter/FilterServiceNames.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Services\Filter;
+
+class FilterServiceNames {
+    
+    const TEXT_BASE_FILTER = 'textBaseFilter';
+    const TEXT_BASE_FILTER_NOT_TOLOWER_CASE = 'textBaseFilterNotToLowerCase';
+    const TEXT_BASE_FILTER_WITH_SQL_RESERVED_WORDS = 'textBaseFilterWithSQLReservedWords';
+}
+
