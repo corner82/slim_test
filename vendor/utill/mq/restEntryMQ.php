994720ca537c4a56d420b7bda7230f96472314bd
corner82 <311corner82@gmail.com>
2015-12-08 12:15:21 +0200

    Message queue Manager ve eklentileri oluşturuldu
    Message queue Manager ve eklentileri oluşturuldu

--- vendor/utill/mq/restEntryMQ.php
+++ vendor/utill/mq/restEntryMQ.php
@@ -15,9 +15,11 @@ class restEntryMQ extends \Utill\MQ\abstractMQ {
     public function basicPublish() {
         if($this->message == null) return null;
         $this->setConnection();
-        $this->channel = $this->connection->channel();
         
-        $this->channel->queue_declare(
+        try {
+           $this->channel = $this->connection->channel();
+        
+            $this->channel->queue_declare(
             $this->channelProperties['queue.name'],    #queue - Queue names may be up to 255 bytes of UTF-8 characters
             $this->channelProperties['passive'],       #passive - can use this to check whether an exchange exists without modifying the server state
             $this->channelProperties['durable'],       #durable, make sure that RabbitMQ will never lose our queue if a crash occurs - the queue will survive a broker restart
@@ -25,14 +27,18 @@ class restEntryMQ extends \Utill\MQ\abstractMQ {
             $this->channelProperties['auto_delete']    #auto delete - queue is deleted when last consumer unsubscribes
             );
             
-        $this->channel->basic_publish(
-            $this->message,     #message 
-            '',                 #exchange
-            $this->channelProperties['queue.name']     #routing key (queue)
-            );
-            //print_r('-------gggggggggg----');
-        $this->channel->close();
-        $this->connection->close();
+            $this->channel->basic_publish(
+                $this->message,     #message 
+                '',                 #exchange
+                $this->channelProperties['queue.name']     #routing key (queue)
+                );
+                //print_r('-------gggggggggg----');
+            $this->channel->close();
+            $this->connection->close(); 
+        } catch (Exception $ex) {
+            
+        }
+        
     }
 
 }
